463c11660ad38a37c9b0a3682ccfc7c5900377d7
Aescosaurus <michaelbuslik@hotmail.com>
2019-03-08 12:53:29 -0700

    Added some TrophyCase methods.

--- HW 5 - Dynamic Trophy Case/TrophyCase.cpp
+++ HW 5 - Dynamic Trophy Case/TrophyCase.cpp
@@ -1,4 +1,5 @@
 #include "TrophyCase.h"
+#include <algorithm>
 
 TrophyCase::TrophyCase()
 	:
@@ -8,12 +9,12 @@ TrophyCase::TrophyCase()
 TrophyCase::TrophyCase(const TrophyCase& rhs)
 	:
 	list(new Trophy*[rhs.capacity]),
-	capacity( rhs.capacity ),
-	curPos( rhs.curPos )
+	capacity(rhs.capacity),
+	curPos(rhs.curPos)
 {
 	for(int i = 0; i < curPos; ++i)
 	{
-		list[i] = new Trophy( *rhs.list[i] );
+		list[i] = new Trophy(*rhs.list[i]);
 	}
 }
 
@@ -52,6 +53,115 @@ void TrophyCase::addTrophy(const string& name, int level,
 		// Assign list to trophies.
 		list = trophies;
 	}
+
+	// TODO: Sort here maybe?
+}
+
+bool TrophyCase::copyTrophy(const string& copyee)
+{
+	const Trophy* foundTrophy = findTrophy(copyee);
+	bool success = false;
+
+	if(foundTrophy != nullptr)
+	{
+		addTrophy(foundTrophy->getName(),
+			foundTrophy->getLevel(),
+			foundTrophy->getColor());
+
+		success = true;
+	}
+	else
+	{
+		success = false;
+	}
+
+	return success;
+}
+
+bool TrophyCase::deleteTrophy(const string& toDelete)
+{
+	Trophy* found = findTrophy(toDelete);
+	bool success = false;
+
+	if(found != nullptr)
+	{
+		// Swap found trophy with the last one.
+		std::swap(*found, *list[curPos]);
+
+		// Pop the found trophy from the list.
+		delete list[curPos];
+		--curPos;
+
+		success = true;
+
+		// TODO: Sort maybe?
+	}
+	else
+	{
+		success = false;
+	}
+
+	return success;
+}
+
+bool TrophyCase::renameTrophy(const string& name,
+	const string& fixedName)
+{
+	Trophy* found = findTrophy(name);
+	bool success = false;
+
+	if(found != nullptr)
+	{
+		found->setName(fixedName);
+
+		success = true;
+	}
+	else
+	{
+		success = false;
+	}
+
+	return success;
+}
+
+bool TrophyCase::relevelTrophy(const string& name,
+	int fixedLevel)
+{
+	Trophy* found = findTrophy(name);
+	bool success = false;
+
+	if(found != nullptr)
+	{
+		found->setLevel(fixedLevel);
+
+		success = true;
+	}
+	else
+	{
+		success = false;
+	}
+
+	return success;
+}
+
+bool TrophyCase::recolorTrophy(const string& name,
+	Color fixedCol)
+{
+	Trophy* match = findTrophy(name);
+	bool success = false;
+
+	if(match != nullptr)
+	{
+		match->setColor(fixedCol);
+
+		success = true;
+	}
+	else
+	{
+		success = false;
+	}
+
+	return success;
 }
 
 int TrophyCase::getNbrOfTrophies() const
@@ -62,4 +172,21 @@ int TrophyCase::getNbrOfTrophies() const
 int TrophyCase::getAllocatedSize() const
 {
 	return capacity;
+}
+
+Trophy* TrophyCase::findTrophy(const string& name)
+{
+	Trophy* result = nullptr;
+
+	for(int i = 0; i < curPos; ++i)
+	{
+		Trophy* curTrophy = list[i];
+
+		if(curTrophy->getName() == name)
+		{
+			result = curTrophy;
+		}
+	}
+
+	return result;
 }
--- HW 5 - Dynamic Trophy Case/TrophyCase.h
+++ HW 5 - Dynamic Trophy Case/TrophyCase.h
@@ -12,9 +12,19 @@ public:
 
 	void addTrophy(const string& name, int level,
 		Color c);
+	bool copyTrophy(const string& copyee);
+	bool deleteTrophy(const string& toDelete);
+	bool renameTrophy(const string& name,
+		const string& fixedName);
+	bool relevelTrophy(const string& name,
+		int fixedLevel);
+	bool recolorTrophy(const string& name,
+		Color fixedCol);
 
 	int getNbrOfTrophies() const;
 	int getAllocatedSize() const;
+private:
+	Trophy* findTrophy(const string& name);
 private:
 	static constexpr int startSize = 10;
 	Trophy** list = nullptr;
